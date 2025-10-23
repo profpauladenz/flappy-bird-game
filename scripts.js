db0fcc3ea06b99549db7dcd399663a493811daac
Paula Hasstenteufel <paula@hasstenteufel.com>
2025-10-23 13:48:01 +0200

    Adicionando elemento "game" no HTML e lógica inicial para START no JS.

---
+++ scripts.js
@@ -0,0 +1,33 @@
+// Estrutura básica de jogo
+const game = document.querySelector('.game');
+const bird = document.querySelector('.bird');
+const hudScore = document.querySelector('.score');
+const message = document.querySelector('.message');
+const btnIniciar = document.querySelector('.start');
+
+// Objeto utilizado como uma classe (em maiúscula)
+const State = {
+    start: "START",
+    play: "PLAY",
+    end: "END"
+}
+
+let state = State.start; 
+
+// HUD - Heads-Up Display
+let score = 0;
+let best = 0; //TODO: Adicionar lógica
+
+//TODO: Adicionar alternativa de início com botão
+// btnIniciar.addEventListener('click', () => {
+//     state = State.play;
+//     message.innerHTML = 'Em jogo!'
+// });
+
+document.addEventListener('keydown', (event) => {
+    if (event.key == "Enter") {
+        state = State.play;
+        console.log("No estado: " + state);
+        message.innerHTML = 'Voa, bruxão!';
+    }
+});
